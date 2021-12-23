9f93607de969223162412d7228f20d2dab89c67d
masakku <masa351012@icloud.com>
2021-12-23 22:10:29 +0900

    Userモデルテストコード修正3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -54,9 +54,13 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
-      it 'passwordは半角英数字混合でなければ登録できない' do
+      it 'passwordは数字のみでは登録できない' do
         @user.password = '000000'
         @user.password_confirmation = '000000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password には半角英字と数字の両方を含めて設定してください')
+      end
+      it 'passwordは英字のみでは登録できない' do
         @user.password = 'aaaaaa'
         @user.password_confirmation = 'aaaaaa'
         @user.valid?
